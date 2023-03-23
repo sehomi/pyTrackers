32db50166ae4aadbd6c5250ace981147cd8e080d
sehomi <sehomi755@gmail.com>
2023-03-23 18:55:26 +0000

    modified draw_ratios.py

--- examples/vis/draw_ratios.py
+++ examples/vis/draw_ratios.py
@@ -7,28 +7,29 @@ except:
 
 if IN_COLAB:
     print("**** in colab ****")
-    if "/content/pyCFTrackers" not in sys.path:
+    if "/content/pyTrackers" not in sys.path:
         print("**** path not set ****")
-        sys.path.insert(0, "/content/pyCFTrackers")
+        sys.path.insert(0, "/content/pyTrackers")
         print(sys.path)
 
 import numpy as np
 import cv2 as cv
 import matplotlib.pyplot as plt
 from matplotlib.offsetbox import TextArea, DrawingArea, OffsetImage, AnnotationBbox
-from lib.utils import get_ground_truthes_viot
+from lib.utils_ import get_ground_truthes_viot
 
-gts=get_ground_truthes_viot('/content/pyCFTrackers/dataset/VIOT/park_mavic_1')[109:190]
+gts=get_ground_truthes_viot('/content/pyTrackers/dataset/VIOT/park_mavic_1')[109:190]
 
-occ = np.loadtxt('/content/pyCFTrackers/results/occ_park_mavic_1.txt')
-kcf_hog = np.loadtxt('/content/pyCFTrackers/results/kcf_park_mavic_1.txt')
-ldes = np.loadtxt('/content/pyCFTrackers/results/ldes_park_mavic_1.txt')
-csrdcf = np.loadtxt('/content/pyCFTrackers/results/csrdcf_park_mavic_1.txt')
-strcf = np.loadtxt('/content/pyCFTrackers/results/strcf_park_mavic_1.txt')
-dimp50 = np.loadtxt('/content/pyCFTrackers/results/dimp50_park_mavic_1.txt')
-kys = np.loadtxt('/content/pyCFTrackers/results/kys_park_mavic_1.txt')
-tomp = np.loadtxt('/content/pyCFTrackers/results/tomp_park_mavic_1.txt')
-prdimp50 = np.loadtxt('/content/pyCFTrackers/results/prdimp50_park_mavic_1.txt')
+occ = np.loadtxt('/content/pyTrackers/results/occ_park_mavic_1.txt')
+kcf_hog = np.loadtxt('/content/pyTrackers/results/kcf_park_mavic_1.txt')
+ldes = np.loadtxt('/content/pyTrackers/results/ldes_park_mavic_1.txt')
+csrdcf = np.loadtxt('/content/pyTrackers/results/csrdcf_park_mavic_1.txt')
+strcf = np.loadtxt('/content/pyTrackers/results/strcf_park_mavic_1.txt')
+dimp50 = np.loadtxt('/content/pyTrackers/results/dimp50_park_mavic_1.txt')
+kys = np.loadtxt('/content/pyTrackers/results/kys_park_mavic_1.txt')
+tomp = np.loadtxt('/content/pyTrackers/results/tomp_park_mavic_1.txt')
+prdimp50 = np.loadtxt('/content/pyTrackers/results/prdimp50_park_mavic_1.txt')
+mixformer = np.loadtxt('/content/pyTrackers/results/mixformer_park_mavic_1.txt')
 
 print('score_kcf ', 1 - np.mean( np.abs(1-occ - kcf_hog) ) )
 print('score_ldes ', 1 - np.mean( np.abs(1-occ - ldes) ) )
@@ -38,6 +39,7 @@ print('score_dimp50 ', 1 - np.mean( np.abs(1-occ - dimp50) ) )
 print('score_kys ', 1 - np.mean( np.abs(1-occ - kys) ) )
 print('score_tomp ', 1 - np.mean( np.abs(1-occ - tomp) ) )
 print('score_prdimp50 ', 1 - np.mean( np.abs(1-occ - prdimp50) ) )
+print('score_mixformer ', 1 - np.mean( np.abs(1-occ - mixformer) ) )
 
 plt.rcParams["figure.figsize"] = (20,6)
 plt.rcParams.update({'font.size': 14})
@@ -53,11 +55,13 @@ ax.plot(dimp50, linewidth=2, label='DiMP50')
 ax.plot(prdimp50, linewidth=2, label='PrDiMP50')
 ax.plot(kys, linewidth=2, label='KYS')
 ax.plot(tomp, linewidth=2, label='ToMP')
+ax.plot(mixformer, linewidth=2, label='MixFormer')
+
 
 for i in range(0, 81, 10):
 
     ax.axvline(x=i, ymin=0.0, ymax=1.0, color='gray', linewidth=1, linestyle='--')
-    img = cv.imread('/content/pyCFTrackers/dataset/VIOT/park_mavic_1/{:08}.jpg'.format(i+110))
+    img = cv.imread('/content/pyTrackers/dataset/VIOT/park_mavic_1/{:08}.jpg'.format(i+110))
     
     x_min = int( np.max([gts[i, 0] - gts[i, 2], 0]) )
     x_max = int( np.min([gts[i, 0] + 2*gts[i, 2], img.shape[1]-1]) )
@@ -81,4 +85,4 @@ ax.set_xlim(-5, 95)
 plt.ylabel('psr/psr0')
 plt.legend()
 plt.grid()
-plt.savefig('VIOT_ratios.pdf', format="pdf")
+plt.savefig('VIOT_ratios_mixformer.pdf', format="pdf")
